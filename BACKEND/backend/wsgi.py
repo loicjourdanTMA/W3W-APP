ddd08f4311fe63a8665eb44be0fa39b46e934737
loicjourdanTMA <loic.jourdan@tma-consultants.co.uk>
2024-12-24 14:50:43 +0000

    Update wsgi.py
    Updated app name in wsgi.py

--- BACKEND/backend/wsgi.py
+++ BACKEND/backend/wsgi.py
@@ -12,7 +12,7 @@ with open(activate_this) as file_:
     exec(file_.read(), dict(__file__=activate_this))
 
 # Set the Django settings module
-os.environ['DJANGO_SETTINGS_MODULE'] = 'your_project_name.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'W3W-APP.settings'
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
