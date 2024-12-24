41daef3cce1ee3cde9bb2e603022e41cf01bf73b
loicjourdanTMA <loic.jourdan@tma-consultants.co.uk>
2024-12-24 14:49:20 +0000

    Update wsgi.py
    Updated the wsgi.py file within the backend

--- BACKEND/backend/wsgi.py
+++ BACKEND/backend/wsgi.py
@@ -1,16 +1,18 @@
-"""
-WSGI config for backend project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+# Path to your project directory
+project_home = '/home/LoicJourdan/W3W-APP'
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
+# Path to the virtual environment
+activate_this = '/home/LoicJourdan/.virtualenvs/myenv/bin/activate_this.py'
+with open(activate_this) as file_:
+    exec(file_.read(), dict(__file__=activate_this))
+
+# Set the Django settings module
+os.environ['DJANGO_SETTINGS_MODULE'] = 'your_project_name.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-
 application = get_wsgi_application()
